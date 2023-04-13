4d53dd1b72ee702901d7a96a965149150b23f53b
Joyugyeong <67578750+Joyugyeong@users.noreply.github.com>
2023-04-13 23:49:00 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 8.96 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public String solution(String s) {
+        String [] arr = s.split(" ");
+        int [] num = new int [arr.length];
+        for(int i=0;i<arr.length;i++)
+        {
+            num[i]=Integer.parseInt(arr[i]); 
+        }
+        Arrays.sort(num);
+        return num[0]+" "+num[num.length-1];
+    }
+}
